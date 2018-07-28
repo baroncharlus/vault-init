86768effa5b7c70963d4471b57907034815bc6d9
baroncharlus <baroncharlus@github.com>
2018-07-28 17:32:58 -0500

    functioning initialization sans encryption of root token

--- vault.go
+++ vault.go
@@ -15,6 +15,17 @@ import (
 	"time"
 )
 
+type InitRequest struct {
+	SecretShares    int `json:"secret_shares"`
+	SecretThreshold int `json:"secret_threshold"`
+}
+
+type InitResponse struct {
+	Keys       []string `json:"keys"`
+	KeysBase64 []string `json:"keys_base64"`
+	RootToken  string   `json:"root_token"`
+}
+
 func main() {
 	log.Println("Starting the vault-init service...")
 
@@ -64,10 +75,8 @@ func main() {
 		case 501:
 			log.Println("Vault is not initialized. Initializing and unsealing...")
 			initialize()
-			unseal()
 		case 503:
 			log.Println("Vault is sealed. Unsealing...")
-			unseal()
 		default:
 			log.Printf("Vault is in an unknown state. Status code: %d", response.StatusCode)
 		}
@@ -75,4 +84,60 @@ func main() {
 		log.Printf("Next check in %s", checkIntervalDuration)
 		time.Sleep(checkIntervalDuration)
 	}
+}
+
+func initialize() {
+	// initialize go struct
+	initRequest := InitRequest{
+		SecretShares:    5,
+		SecretThreshold: 3,
+	}
+
+	// convert go struct to json
+	initRequestData, err := json.Marshal(&initRequest)
+	if err != nil {
+		log.Println(err)
+		return
+	}
+
+	// create web request to vault init endpoint
+	r := bytes.NewReader(initRequestData)
+	request, err := http.NewRequest("PUT", vaultAddr+"/v1/sys/init", r)
+	if err != nil {
+		log.Println(err)
+		return
+	}
+
+	// send request; close socket once we're done
+	response, err := httpClient.Do(request)
+	if err != nil {
+		log.Println(err)
+		return
+	}
+	defer response.Body.Close()
+
+	// capture response body as raw json
+	initRequestResponseBody, err := ioutil.ReadAll(response.Body)
+	if err != nil {
+		log.Println(err)
+		return
+	}
+
+	// log whether vault was initialized or not
+	if response.StatusCode != 200 {
+		log.Printf("init: non 200 status code: %d", response.StatusCode)
+		return
+	}
+
+	// initialize var initResponse of type InitResponse
+	var initResponse InitResponse
+
+	// cast the raw json response body back to our initialized but otherwise
+	// empty initResponse variable. Log if this fails.
+	if err := json.Unmarshal(initRequestResponseBody, &initResponse); err != nil {
+		log.Println(err)
+		return
+	}
+
+	fmt.Println(string(initRequestResponseBody))
 }
