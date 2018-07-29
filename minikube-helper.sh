3feb4ed8e19043ba293b301c20e85db7de884a89
baroncharlus <baroncharlus@github.com>
2018-07-28 21:55:33 -0500

    replacing gcs storage backend with a hostPath for testing purposes

--- minikube-helper.sh
+++ minikube-helper.sh
@@ -7,7 +7,7 @@ VAULT_VER=$1
 MINIKUBE_PID=$(pidof VBoxHeadless)
 
 minikube delete
-rm -rf ~/.minikube
+rm -rf ~/.minikube/
 
 if [[ -z $MINIKUBE_PID ]]; then
   pkill VBoxHeadless
@@ -17,5 +17,10 @@ minikube start
 kubectl config use-context minikube
 eval "$(minikube docker-env)"
 docker build -t vault-go:"${VAULT_VER}" .
+kubectl create secret generic vault \
+  --from-file=ca.pem \
+  --from-file=vault.pem \
+  --from-file=vault-combined.pem \
+  --from-file=vault-key.pem
 kubectl create -f vault.yaml
-kubectl rollout status deployment/vault
+kubectl rollout status statefulset/vault
